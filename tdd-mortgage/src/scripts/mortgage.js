19c25a197a9ee8fe131374b1d01725acd8683f9a
mismatchmile <mtimms@live.ca>
2020-03-24 17:19:54 -0600

    completed TDD codealong

--- tdd-mortgage/src/scripts/mortgage.js
+++ tdd-mortgage/src/scripts/mortgage.js
@@ -11,16 +11,30 @@ const mortFunctions = {
         const interest = yearInterest / 100 / 12;
         const payments = years * 12;
 
-        // Now compute the monthly payment figure
+        // Now compute the monthly payment figure, using esoteric math.
         const x = Math.pow(1 + interest, payments);
         const monthly = (principal * x * interest) / (x - 1);
         const total = monthly * payments;
         const totalInterest = total - principal;
 
+        //
+        // If we have valid number return them
+        //
+        if (monthly & total) {
+            return {
+                monthly: mortFunctions.round(monthly),
+                total: mortFunctions.round(total),
+                interest: mortFunctions.round(totalInterest)
+            }
+        }
+
+        // 
+        // The numbers must not be valid
+        //
         return {
-            monthly: mortFunctions.round(monthly),
-            total: mortFunctions.round(total),
-            interest: mortFunctions.round(totalInterest)
+            monthly: '',
+            total: '',
+            interest: '',
         }
     },
 
---
+++ tdd-mortgage/src/scripts/play.js
@@ -0,0 +1,9 @@
+  function calcLoan(principal, yearInterest, years) {
+
+      return {
+          monthly: 0,
+          total: 0,
+          interest: 0,
+      }
+
+  }
